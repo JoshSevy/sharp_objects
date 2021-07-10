45fcb253eb71c48b4fd290047a6fc415fde96809
JoshSevy <joshuasevy@outlook.com>
2021-07-10 17:31:18 -0600

    Fix bug now able to save to git

--- sharp_objects/sharp_objects/Program.cs
+++ sharp_objects/sharp_objects/Program.cs
@@ -18,7 +18,29 @@ namespace sharp_objects
             int num2 = 23;
             int sum = num1 + num2;
 
-            Console.WriteLine(sum);
+            // Print to console using concatination
+
+            Console.WriteLine("num1: " + num1 + " + num2: " + num2 + " is " + sum);
+
+            // doubles 
+
+            double d1 = 3.1215;
+            double d2 = 5.1;
+            double dDiv = d1 / d2;
+
+            Console.WriteLine("d1 / d2 is " + dDiv);
+
+
+            // Same example using floats
+
+            float f1 = 3.1215f;
+            float f2 = 5.1f;
+            float fDiv = f1 / f2;
+
+            Console.WriteLine("f1 / f2 is " + fDiv);
+
+
+
             Console.Read();
         }
     }
